67690e4e0d8c51276c187d96be6c0355edbb427a
AdielsonMMartins <adielson@outlook.com>
2023-05-25 04:56:11 -0300

    fix: import de AnimationsModule para melhor funcionamento da PrimeNg

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,7 +15,9 @@ import { CabecalhoComponent } from './views/cabecalho/cabecalho.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
