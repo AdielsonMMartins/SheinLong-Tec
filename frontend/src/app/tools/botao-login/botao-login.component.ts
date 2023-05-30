1d2d2c0ad78ab90bcd2997b6733256d96083bda6
AdielsonMMartins <adielson@outlook.com>
2023-05-30 04:40:43 -0300

    feat: implementação do botao de login

--- frontend/src/app/tools/botao-login/botao-login.component.ts
+++ frontend/src/app/tools/botao-login/botao-login.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-botao-login',
   templateUrl: './botao-login.component.html',
   styleUrls: ['./botao-login.component.scss']
 })
-export class BotaoLoginComponent {
+export class BotaoLoginComponent implements OnInit{
+
+  items!: MenuItem[];
+    
+  ngOnInit() {
+      this.items = [
+          {
+              label: 'Cliente',
+              icon: 'pi pi-fw pi-circle',
+              
+          },
+          {
+              label: 'Funcionário',
+              icon: 'pi pi-fw pi-circle',
+              
+          },
+          
+      ];
+  }
+
+
 
 }
